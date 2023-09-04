0e203145379d8ceae13fe1ae5e5a472735ec252d
muhammetkrkrt <karakurt3656@gmail.com>
2023-09-04 14:10:04 +0300

    enum-status-auth-user-service entity added

---
+++ user-microservice/src/main/java/com/bilgeadam/repository/entity/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.bilgeadam.repository.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.MappedSuperclass;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+public class BaseEntity {
+    private Long createdDate;
+    private Long updatedDate;
+}
