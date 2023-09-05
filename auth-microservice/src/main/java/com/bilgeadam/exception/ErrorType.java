ca432ad60153b4e2283342b018664a9baabe165e
siinemvarol <siinemvarol@gmail.com>
2023-09-05 15:36:00 +0300

    login added

--- auth-microservice/src/main/java/com/bilgeadam/exception/ErrorType.java
+++ auth-microservice/src/main/java/com/bilgeadam/exception/ErrorType.java
@@ -11,6 +11,8 @@ public enum ErrorType {
     VALIDATION_ERROR(3000,"",HttpStatus.BAD_REQUEST),
     BAD_REQUEST(4000,"", HttpStatus.BAD_REQUEST),
     USERNAME_DUPLICATE(4002,"User already exist" ,HttpStatus.BAD_REQUEST),
+    USER_NOT_FOUND(4003,"User not found" ,HttpStatus.BAD_REQUEST),
+    ACCOUNT_NOT_ACTIVE(4004,"Account is not active" ,HttpStatus.BAD_REQUEST),
     INTERNAL_ERROR(5000,"Internal Server Error",HttpStatus.INTERNAL_SERVER_ERROR),
     PASSWORDS_NOT_MATCH(4007,"Passwords do not match",HttpStatus.BAD_REQUEST);
 
