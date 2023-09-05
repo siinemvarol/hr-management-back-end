1e0b4034bdc073b6ebca41942e6df201656321ae
muhammetkrkrt <karakurt3656@gmail.com>
2023-09-05 11:14:09 +0300

    register-create$3 added

--- auth-microservice/src/main/java/com/bilgeadam/exception/ErrorType.java
+++ auth-microservice/src/main/java/com/bilgeadam/exception/ErrorType.java
@@ -11,7 +11,8 @@ public enum ErrorType {
     VALIDATION_ERROR(3000,"",HttpStatus.BAD_REQUEST),
     BAD_REQUEST(4000,"", HttpStatus.BAD_REQUEST),
     USERNAME_DUPLICATE(4002,"User already exist" ,HttpStatus.BAD_REQUEST),
-    INTERNAL_ERROR(5000,"Internal Server Error",HttpStatus.INTERNAL_SERVER_ERROR);
+    INTERNAL_ERROR(5000,"Internal Server Error",HttpStatus.INTERNAL_SERVER_ERROR),
+    PASSWORDS_NOT_MATCH(4007,"Passwords do not match",HttpStatus.BAD_REQUEST);
 
     private int code;
     private String message;
