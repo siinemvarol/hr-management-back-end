4982c0a0e026f6fba0916f8c4b21591ce5833014
ayse-ozcan <ayseozcan.1@icloud.com>
2023-09-19 14:20:49 +0300

    api-gateway updated

--- api-gateway-service/src/main/java/com/bilgeadam/ApiGatewayApplication.java
+++ api-gateway-service/src/main/java/com/bilgeadam/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.bilgeadam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin("*")
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class);
