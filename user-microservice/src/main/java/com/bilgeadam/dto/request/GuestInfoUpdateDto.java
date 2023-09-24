d6ee6b43e88552e41936748c402d30e0f5b79202
muhammetkrkrt <karakurt3656@gmail.com>
2023-09-25 02:30:27 +0300

    guest-info

---
+++ user-microservice/src/main/java/com/bilgeadam/dto/request/GuestInfoUpdateDto.java
@@ -0,0 +1,33 @@
+package com.bilgeadam.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GuestInfoUpdateDto {
+    @NotEmpty (message = "Name field cannot be empty")
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters.")
+    private String name;
+
+    @NotEmpty(message = "Surname field cannot be empty")
+    @Size(min = 3, max = 20, message = "Surname must be between 3 and 20 characters.")
+    private String surname;
+
+
+    @NotEmpty(message = "Phone field cannot be empty" )
+    private String phone;
+
+    @NotEmpty(message = "Address field cannot be empty" )
+    private String address;
+
+    private String personalEmail;
+}
+
